f5c21fb41c12f19c0ef48ab80be9177764af924b
datacamp-content-creator <32279578+datacamp-content-creator@users.noreply.github.com>
2019-05-21 11:47:45 +0200

    Update requirements.sh from editor

--- requirements.sh
+++ requirements.sh
@@ -1,19 +1,15 @@
 # If bash command fails, build should error out
 set -e
 
-# Install specific package versions with pip
-# Check the latest version using https://pypi.org/search
+##### Install specific package versions with pip #####
+pip3 install spacy>="2.1.0,<2.2.0"
 
-# pip3 install pandas==0.23.3
-# pip3 install matplotlib==2.2.2
-# pip3 install scikit-learn==0.19.2
-# pip3 install numpy==1.15.0
-# pip3 install seaborn==0.9.0
-# pip3 install bokeh==0.13.0
+# download english model and create link
+pip3 install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.1.0/en_core_web_sm-2.1.0.tar.gz
+pip3 install https://github.com/explosion/spacy-models/releases/download/de_core_news_sm-2.1.0/de_core_news_sm-2.1.0.tar.gz
+python3 -m spacy link en_core_web_sm en
+python3 -m spacy link de_core_news_sm de
 
-
-# Include datasets in the image
-
-# DATADIR=/usr/local/share/datasets
-# mkdir -p $DATADIR
-# wget -O $DATADIR/iris.csv http://s3.amazonaws.com/assets.datacamp.com/staging/course_2406/datasets/iris.csv
+# install custom build of en_core_web_md model that loads faster
+wget -nv https://s3.amazonaws.com/assets.datacamp.com/production/repositories/1496/datasets/0db325df3377d8d9086e2aee29994ad158d9858cd/en_core_web_md-2.1.0.tar.gz
+pip3 install /home/repl/en_core_web_md-2.1.0.tar.gz
